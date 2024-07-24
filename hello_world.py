2583834955a914662525a0405cc62d5824ddff76
Vanleealberts <vanleealberts969@gmail.com>
2024-07-24 15:57:40 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,4 @@
+name = input("vanlee")
+age = input("19")
+print(name,age) 
+print("Hello world")
---
+++ menu.py
@@ -0,0 +1,8 @@
+item_1 = input("what would you like to order")
+item_2 = input("what would you like to order")
+item_3 = input("what would you like to order")
+
+print("Order Confirmation! You have ordered:")
+print(item_1)
+print(item_2)
+print(item_3)
